6f813f74a2782b7670c063fba619e2074642debc
PeachScript <scdzwyxst@gmail.com>
2021-03-08 19:10:19 +0800

    chore: initialize repository

---
+++ .umirc.ts
@@ -0,0 +1,15 @@
+export default {
+  plugins: ['./lib/index.js'],
+  hire: {
+    title: 'Waiting for you',
+    content: `
+<p>Job Description:</p>
+<ul>
+  <li>balabala</li>
+  <li>balabala</li>
+  <li>balabala</li>
+</ul>`,
+    email: 'youremail@example.com',
+    slogan: 'Find a nice job?',
+  }
+}
